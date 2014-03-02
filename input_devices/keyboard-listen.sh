567299edbc1d180266432a010d1f1c844e767f0e
muflax <mail@muflax.com>
2014-03-02 09:26:30 +0000

    silent killing

--- input_devices/keyboard-listen.sh
+++ input_devices/keyboard-listen.sh
@@ -2,5 +2,5 @@
 # Copyright muflax <mail@muflax.com>, 2014
 # License: GNU GPL 3 <http://www.gnu.org/copyleft/gpl.html>
 
-killall netcat
+killall -q netcat
 netcat --listen --local-port=4444 >> /dev/input/by-path/platform-i8042-serio-0-event-kbd &
