15012a5a509f92be7b37f1c1c7a62dee5342aabd
muflax <mail@muflax.com>
2014-06-01 04:11:39 +0100

    evernote backup

---
+++ evernote-backup.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+# Copyright muflax <mail@muflax.com>, 2014
+# License: GNU GPL 3 <http://www.gnu.org/copyleft/gpl.html>
+
+DISPLAY=:0.0 $HOME/local/bin/nixnote --syncAndExit >/dev/null 2>/dev/null
