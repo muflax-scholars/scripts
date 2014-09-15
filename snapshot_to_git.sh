db8a1f08f501da9540a8ac5731094107febeb286
muflax <mail@muflax.com>
2014-09-15 03:58:27 +0100

    tarballs -> git commits

---
+++ snapshot_to_git.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+# Copyright muflax <mail@muflax.com>, 2014
+# License: GNU GPL 3 <http://www.gnu.org/copyleft/gpl.html>
+
+echo "using $(pwd) as final repo..."
+sleep 3
+git init
+
+export GIT_DIR=$PWD/.git
+
+for dir in $*; do
+  cd $dir
+  git add -A .
+  git commit -m "import $dir into git"
+done
+
+echo "done!"
