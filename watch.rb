f027c36f680d24a3f5d58d8a859ed19cf2ea81cf
muflax <mail@muflax.com>
2014-10-25 13:52:14 +0100

    add option to force restarts instead of waiting for the process to finish

--- watch.rb
+++ watch.rb
@@ -15,8 +15,9 @@ opts = Trollop::options do
   opt :changes,	"watch path for changes",      	:type => :string,	:multi => true
   opt :notify, 	"send notification on changes",	:type => :string
 
-  opt :recursive,	"check paths recursively for changes",	:default => true
-  opt :clear,    	"clear screen upon refresh",          	:default => true
+  opt :recursive,	"check paths recursively for changes",           	:default => true
+  opt :clear,    	"clear screen upon refresh",                     	:default => true
+  opt :kill,     	"kills and restarts process if the path changes",	:default => false
 
   opt :quiet,	"don't show status messages"
 
@@ -28,7 +29,6 @@ Trollop::die "no command specified" if ARGV.empty?
 
 # wait either for the specified time, or 1 second if not in --changes mode
 wait = opts[:wait] || (opts[:changes].empty? ? 1 : 0)
-
 
 def watch files, recursive: true
   notifier = INotify::Notifier.new
@@ -65,26 +65,35 @@ begin
         system "clear"
       end
 
-      start = Time.now
-      system "zsh -l -c '#{ARGV.join(" ")}'"
-      runtime = Time.now - start
+      process = Thread.new do
+        start = Time.now
+        system "zsh -l -c '#{ARGV.join(" ")}'"
+        runtime = Time.now - start
 
-      if opts[:notify]
-        system "notify-send '#{opts[:notify]}'"
+        if opts[:notify]
+          system "notify-send '#{opts[:notify]}'"
+        end
+
+        duration = if runtime >= 1.hour
+                     "%dh%02dm%02ds" % [runtime / 1.hour, runtime % 1.hour, runtime % 1.minute]
+                   elsif runtime >= 1.minute
+                     "%dm%02ds" % [runtime / 1.minute, runtime % 1.minute]
+                   else
+                     "%ds" % runtime
+                   end
+
+        print "[WAITING (#{duration})]..." unless opts[:quiet]
       end
 
-      duration = if runtime >= 1.hour
-                   "%dh%02dm%02ds" % [runtime / 1.hour, runtime % 1.hour, runtime % 1.minute]
-                 elsif runtime >= 1.minute
-                   "%dm%02ds" % [runtime / 1.minute, runtime % 1.minute]
-                 else
-                   "%ds" % runtime
-                 end
-
-      print "[WAITING (#{duration})]..." unless opts[:quiet]
+      # wait for the process by default
+      process.join unless opts[:kill]
 
       if not opts[:changes].empty?
-        watch opts[:changes], recursive: opts[:recursive]
+        watchdog = Thread.new do
+          watch opts[:changes], recursive: opts[:recursive]
+        end
+
+        watchdog.join
       end
 
       sleep(wait)
@@ -95,6 +104,10 @@ begin
       sleep(1)
     end
 
+    # make sure threads are cleanly stopped once we restart
+    Thread.list.select{|t| t != Thread.current}.each do |thread|
+      thread.kill
+    end
   end
 rescue Interrupt
   # end for real
